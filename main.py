af7b4b95ae8b6abb55ba4cbae632e0e04a7f844f
Jonas Rocha Lima Amaro <jonasrla@gmail.com>
2021-05-23 00:01:38 -0300

    Deduplica registros

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from pyspark.sql import SparkSession
+from pyspark.sql.window import Window                                       
+from pyspark.sql import functions as f
 
 spark = SparkSession.builder \
             .master('local') \
@@ -6,5 +8,8 @@ spark = SparkSession.builder \
             .getOrCreate()
 
 df = spark.read.csv('data/input/users/load.csv', header=True)
+
+windowId = Window.partitionBy('id').orderBy(f.col('update_date').desc())
+df.withColumn("row", f.row_number().over(windowId)).filter(f.col('row') == 1).drop('row').show()
 
 df.write.parquet('data/output/users')

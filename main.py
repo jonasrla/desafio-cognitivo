c6072674ab23ef9f2c019f54fd70833097182fec
Jonas Rocha Lima Amaro <jonasrla@gmail.com>
2021-05-23 00:32:02 -0300

    converte o tipo das colunas

--- main.py
+++ main.py
@@ -9,6 +9,12 @@ spark = SparkSession.builder \
 
 df = spark.read.csv('data/input/users/load.csv', header=True)
 
+df = df.withColumn('id', df.id.astype('int'))
+df = df.withColumn('age', df.age.astype('int'))
+
+df = df.withColumn('create_date', f.to_timestamp(df.create_date))
+df = df.withColumn('update_date', f.to_timestamp(df.update_date))
+
 windowId = Window.partitionBy('id').orderBy(f.col('update_date').desc())
 df = df.withColumn("row", f.row_number().over(windowId)).filter(f.col('row') == 1).drop('row')
 

16e0465edabc73e32c813692c8110d5611d9663d
Jonas Rocha Lima Amaro <jonasrla@gmail.com>
2021-05-24 17:15:23 -0300

    Atualiza formato de saída

--- app/main.py
+++ app/main.py
@@ -18,4 +18,4 @@ df = df.withColumn('update_date', f.to_timestamp(df.update_date))
 windowId = Window.partitionBy('id').orderBy(f.col('update_date').desc())
 df = df.withColumn("row", f.row_number().over(windowId)).filter(f.col('row') == 1).drop('row')
 
-df.write.parquet('data/output/users')
+df.write.orc('data/output/users', compression='zlib')
